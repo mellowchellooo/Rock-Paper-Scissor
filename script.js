51f7c0175e37d74ec01f264631055bd4bc98edeb
Marcello <marcellomino@gmail.com>
2025-12-12 12:40:27 -0500

    added a function to get human choice with a prompt function. wrote an if..else statement to get different results depending on humanchoice

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let computerMove = ''; /* computerMove will be used in other parts so I let it stay global. I have empty strings that will be filled in with the computers move depending on the random number it generates */
 
+
 function getComputerChoice(){
     let randomNum = Math.floor(Math.random() * 3);/*I want randomNum to be between 0 and 2 for my three different choices. I use math.random to generate float between 0-1 multiply by three to get a number between 0-3. then math.floor rounds DOWN so my only numbers are 0, 1, 2. and no decimal numbers */
     
@@ -18,6 +19,20 @@ function getComputerChoice(){
     and then return answer when getComputerChoice() is called
     */
 }
+function humanChoice() {
+  let humanChoice = prompt('rock, paper, or scissor?');
+  humanChoice = humanChoice.toLowerCase();
 
-console.log(getComputerChoice());
+  if (
+    humanChoice === 'rock' ||
+    humanChoice === 'paper' ||
+    humanChoice === 'scissor'
+  ) {
+    console.log(humanChoice);
+    return humanChoice;
+  } else {
+    console.log ('choose wisely, reload page and try again');
+  }
+}
 
+humanChoice();
