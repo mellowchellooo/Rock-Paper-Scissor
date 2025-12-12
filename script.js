39fdb13fda09a34efe3a555a9f85f9a1c60a9d78
Marcello <marcellomino@gmail.com>
2025-12-12 13:07:13 -0500

    created a function to make a round of rock paper scissor between human choice, and computer'

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let computerMove = ''; /* computerMove will be used in other parts so I let it stay global. I have empty strings that will be filled in with the computers move depending on the random number it generates */
 
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     let randomNum = Math.floor(Math.random() * 3);/*I want randomNum to be between 0 and 2 for my three different choices. I use math.random to generate float between 0-1 multiply by three to get a number between 0-3. then math.floor rounds DOWN so my only numbers are 0, 1, 2. and no decimal numbers */
@@ -19,9 +21,9 @@ function getComputerChoice(){
     and then return answer when getComputerChoice() is called
     */
 }
-function humanChoice() {
-  let humanChoice = prompt('rock, paper, or scissor?');
-  humanChoice = humanChoice.toLowerCase();
+function  getHumanChoice() {
+  let humanChoice = prompt('rock, paper, or scissor?'); /* a prompt will pop up with the words rock,paper,scissor inside of it. whatever you write in the prompt box will be stored as a string in the "humanChoice variable"*/
+  humanChoice = humanChoice.toLowerCase();/* we are making sure that the value stored in humanChoice, will always be lowercase, because everything is case sensitive. by putting .toLowerCase, on the variable, it makes it so all letters in the string are lowercase.*/
 
   if (
     humanChoice === 'rock' ||
@@ -34,5 +36,28 @@ function humanChoice() {
     console.log ('choose wisely, reload page and try again');
   }
 }
+/*the if..else statement makes it so if your input in prompt is strictly equal to 'rock'
+'paper' or(||) 'scissor' it console.logs the human choice
+and returns it as well.
+but if you write anything other than rock, paper, or scissor,
+it will console.log the string 'choose wisely...etc.*/
 
-humanChoice();
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === computerChoice){
+        console.log('It"s a tie!');
+    } else if (humanChoice === 'rock' && computerChoice === 'scissor'){
+        console.log('You Win!');
+    } else if (humanChoice === 'paper' && computerChoice === 'rock'){
+        console.log('You Win!');
+    } else if(humanChoice === 'scissor' && computerChoice === 'paper'){
+        console.log('You Win!');
+    } else {
+        console.log('LAHOOOO..SAAAHERRRR');
+    }
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
