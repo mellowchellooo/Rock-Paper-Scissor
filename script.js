b22f64b445476e096af38d95bf190cfbdafea5b6
Marcello <marcellomino@gmail.com>
2025-12-15 10:58:11 -0500

    reworked the code to make it cleaner and finished making 5 rounds*/

--- script.js
+++ script.js
@@ -1,70 +1,87 @@
-let computerMove = ''; /* computerMove will be used in other parts so I let it stay global. I have empty strings that will be filled in with the computers move depending on the random number it generates */
+/* this is to get computer choice*/
 
-let humanScore = 0;
-let computerScore = 0;
+function getComputerChoice() {
+  let randomNum = Math.floor(Math.random() * 3);/*math.floor is used to get a whole number from math.random * 3 and compare r,p,s to 0,1,2*/
 
-function getComputerChoice(){
-    let randomNum = Math.floor(Math.random() * 3);/*I want randomNum to be between 0 and 2 for my three different choices. I use math.random to generate float between 0-1 multiply by three to get a number between 0-3. then math.floor rounds DOWN so my only numbers are 0, 1, 2. and no decimal numbers */
-    
-    if (randomNum === 0){
-        computerMove = 'rock';
-    } else if(randomNum === 1){
-        computerMove = 'paper';
-    } else if(randomNum === 2){
-        computerMove = 'scissor';
-    }
-   return computerMove;
-
-    /*
-     depending what random number generates, 
-    it will be assigned a certain string value,
-    and then return answer when getComputerChoice() is called
-    */
+  if (randomNum === 0) {
+    return 'rock';
+  } else if (randomNum === 1) {
+    return 'paper';
+  } else {
+    return 'scissor';
+  }
 }
-function  getHumanChoice() {
-  let humanChoice = prompt('rock, paper, or scissor?'); /* a prompt will pop up with the words rock,paper,scissor inside of it. whatever you write in the prompt box will be stored as a string in the "humanChoice variable"*/
-  humanChoice = humanChoice.toLowerCase();/* we are making sure that the value stored in humanChoice, will always be lowercase, because everything is case sensitive. by putting .toLowerCase, on the variable, it makes it so all letters in the string are lowercase.*/
+/*this will get the human choice from the input and return the input*/
+function getHumanChoice() {
+  let humanChoice = prompt('rock, paper, or scissor?');
+
+  if (!humanChoice) {
+    console.log('No input');
+    return null;
+  }
+
+  humanChoice = humanChoice.toLowerCase();
 
   if (
     humanChoice === 'rock' ||
     humanChoice === 'paper' ||
     humanChoice === 'scissor'
   ) {
-    console.log(humanChoice);
     return humanChoice;
   } else {
-    console.log ('choose wisely, reload page and try again');
+    console.log('choose wisely, reload page and try again');
+    return null;
   }
 }
-/*the if..else statement makes it so if your input in prompt is strictly equal to 'rock'
-'paper' or(||) 'scissor' it console.logs the human choice
-and returns it as well.
-but if you write anything other than rock, paper, or scissor,
-it will console.log the string 'choose wisely...etc.*/
 
 
-function playRound(humanChoice, computerChoice){
-    if (humanChoice === computerChoice){
-        console.log('It"s a tie!');
-    } else if (humanChoice === 'rock' && computerChoice === 'scissor'){
-        console.log('You Win!');
-    } else if (humanChoice === 'paper' && computerChoice === 'rock'){
-        console.log('You Win!');
-    } else if(humanChoice === 'scissor' && computerChoice === 'paper'){
-        console.log('You Win!');
-    } else {
-        console.log('LAHOOOO..SAAAHERRRR');
-    }
+/*this gets the human choice and computer choice and compares them to see who won the round*/
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+    return 'tie';
+  }
+
+  if (
+    (humanChoice === 'rock' && computerChoice === 'scissor') ||
+    (humanChoice === 'paper' && computerChoice === 'rock') ||
+    (humanChoice === 'scissor' && computerChoice === 'paper')
+  ) {
+    console.log('You Win!');
+    return 'winner';
+  } else {
+    console.log('LAHOOOO.. SAAAHERRRR');
+    return 'loser';
+  }
 }
-/* this function takes two parameters. and gets the human choice to compare against
-one of three different choices the computer has.
-if the human writes rock paper or scissor, and the computer chose one of the options
-written the human wins and it logs the winner message on the screen.
-if its anything other than rock paper or scissor it console.logs loser like ace ventura says it. 
-**it will also write loser if you write anything other than rock paper or scissor. 
-*/
 
-let humanSelection = getHumanChoice(); /*assigns whatever the humanchoice was in this function to the variable"*/
-let computerSelection = getComputerChoice();/*assigns computerChoice to variable*/
+/*this plays the game for 5 rounds and makes a score*/
+function playGame(round, humanScore, computerScore) {
+  if (round > 5) {
+    console.log('--- GAME OVER ---');
+    console.log('Human:', humanScore);
+    console.log('Computer:', computerScore);
+    return;
+  }
 
-playRound(humanSelection, computerSelection);/*parameter taken were the variables that contain "answers" for the functions they are assigned*/
+  console.log(`--- Round ${round} ---`);
+
+  let humanSelection = getHumanChoice();
+  if (humanSelection === null) return;
+
+  let computerSelection = getComputerChoice();
+  console.log('Computer chose:', computerSelection);
+
+  let result = playRound(humanSelection, computerSelection);
+
+  if (result === 'winner') {
+    humanScore = humanScore + 1;
+  } else if (result === 'loser') {
+    computerScore = computerScore + 1;
+  }
+
+  playGame(round + 1, humanScore, computerScore);
+}
+
+
+playGame(1, 0, 0);
